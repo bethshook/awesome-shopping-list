c7f6710844bd06ab3a32e5521f47602f07ca0e51
Beth Shook <eshook@wearenolte.com>
2021-02-07 00:53:43 -0600

    Add logic for adding pending items and filtering items

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-const Wrapper = styled.div``;
-const FlexSection = styled.div``;
+const Wrapper = styled.main``;
+const FlexSection = styled.section``;
 const FlexItem = styled.div``;
 const ListGroup = styled.div``;
+const Input = styled.input``;
+const Button = styled.button``;
+
+const H1 = styled.h1``;
+const H2 = styled.h2``;
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [pendingItems, setPendingItems] = useState([]);
+  const [visiblePendingItems, setVisiblePendingItems] = useState([]);
+
+  useEffect(() => {
+    setVisiblePendingItems(pendingItems);
+  }, [pendingItems])
+
+  const handleChange = input => {
+    setInputValue(input);
+    if (!input) {
+      setVisiblePendingItems(pendingItems);
+    } else {
+      const matchingPending = pendingItems.filter(item => item.includes(input));
+      setVisiblePendingItems(matchingPending);
+    }
+  }
+
   return (
     <Wrapper>
-      <h1>Awesome Shopping List App</h1>
+      <H1>Awesome Shopping List App</H1>
       <FlexSection>
         {/* Input and subtotal go at top */}
+        <Input value={inputValue} placeholder="Item name" onChange={(e)  => handleChange(e.target.value)}/>
+        <Button onClick={() => setPendingItems([...pendingItems, inputValue])}>Create</Button>
+        Subtotal (USD): $0.00
       </FlexSection>
 
       <FlexSection>
         <FlexItem>
-          {/* Pending items go here */}
-          {/* Mapped ListGroups */}
+          <H2>Pending</H2>
+          {/* For each category create a list group */}
+          {/* Mapped categories */}
+          <ul>
+            {visiblePendingItems.map(item => (
+              <li>{item}</li>
+            ))}
+          </ul>
         </FlexItem>
         <FlexItem>
+          <H2>Crossed Off</H2>
           {/* Crossed off items go here */}
           {/* Only one list group bc no categories shown */}
           <ListGroup />
