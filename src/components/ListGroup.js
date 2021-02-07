eeb48e2c599d17964a52de5415d0454c6abcdb6b
Beth Shook <eshook@wearenolte.com>
2021-02-07 02:35:43 -0600

    Implement modal library

--- src/components/ListGroup.js
+++ src/components/ListGroup.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Modal from "styled-react-modal";
 
 const Wrapper = styled.div``;
 const H3 = styled.h3``;
 const List = styled.ul``;
 const ListItem = styled.li``;
 const EditButton = styled.button``;
+const StyledModal = styled(Modal)``;
 
 function ListGroup({ title, items, itemClickHandler, itemEditHandler }) {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const sortByLabel = (a,b) => {
-        const labelA = a.label.toUpperCase();
-        const labelB = b.label.toUpperCase();
-        return (labelA > labelB) ? 1 : ((labelB > labelA) ? -1 : 0);
-    }
+  const toggleModal = (e) => {
+    setIsOpen(!isOpen);
+  };
+
+  const sortByLabel = (a, b) => {
+    const labelA = a.label.toUpperCase();
+    const labelB = b.label.toUpperCase();
+    return labelA > labelB ? 1 : labelB > labelA ? -1 : 0;
+  };
 
   return (
     <Wrapper>
@@ -25,7 +32,19 @@ function ListGroup({ title, items, itemClickHandler, itemEditHandler }) {
             <ListItem onClick={() => itemClickHandler(item)}>
               {item.qty} {item.label} ${item.price}.00
             </ListItem>
-            <EditButton onClick={() => itemEditHandler(item)}>edit</EditButton>
+            {typeof itemEditHandler === "function" ? (
+              <>
+                <EditButton onClick={() => toggleModal()}>edit</EditButton>
+                <StyledModal
+                  isOpen={isOpen}
+                  onBackgroundClick={toggleModal}
+                  onEscapeKeydown={toggleModal}
+                >
+                  <p>{item.label} {item.category} {item.price}</p>
+                  <button onClick={toggleModal}>Close me</button>
+                </StyledModal>
+              </>
+            ) : null}
           </React.Fragment>
         ))}
       </List>
@@ -34,17 +53,17 @@ function ListGroup({ title, items, itemClickHandler, itemEditHandler }) {
 }
 
 ListGroup.propTypes = {
-    title: PropTypes.string,
-    items: PropTypes.arrayOf(PropTypes.object),
-    itemClickHandler: PropTypes.func,
-    itemEditHandler: PropTypes.func,
+  title: PropTypes.string,
+  items: PropTypes.arrayOf(PropTypes.object),
+  itemClickHandler: PropTypes.func,
+  itemEditHandler: PropTypes.func,
 };
 
 ListGroup.defaultProps = {
-    title: "",
-    items: [],
-    itemClickHandler: () => {},
-    itemEditHandler: () => {},
+  title: "",
+  items: [],
+  itemClickHandler: () => {},
+  itemEditHandler: null,
 };
 
 export default ListGroup;
