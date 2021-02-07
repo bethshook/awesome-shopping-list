4b156fcf9339176768991c38a8566f98bb3ffc7a
Beth Shook <eshook@wearenolte.com>
2021-02-07 02:18:30 -0600

    Add proptypes and fix for mapped item keys

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import ListGroup from './components/ListGroup';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import ListGroup from "./components/ListGroup";
 
 const Wrapper = styled.main``;
 const FlexSection = styled.section``;
@@ -12,12 +12,13 @@ const H1 = styled.h1``;
 const H2 = styled.h2``;
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
+  const [itemCount, setItemCount] = useState(0);
+  const [inputValue, setInputValue] = useState("");
   const [pendingItems, setPendingItems] = useState([]);
   const [visiblePendingItems, setVisiblePendingItems] = useState([]);
   const [removedItems, setRemovedItems] = useState([]);
-  const [categories, setCategories] = useState(['uncategorized']);
-  const [itemsByCategory, setItemsByCategory] = useState({'uncategorized': []});
+  const [categories, setCategories] = useState(["uncategorized"]);
+  const [itemsByCategory, setItemsByCategory] = useState({ uncategorized: [] });
 
   useEffect(() => {
     // Changes made to pending reset visible pending items.
@@ -27,39 +28,47 @@ function App() {
   useEffect(() => {
     // Update sorted visible items when visible items changes.
     const categoryObj = {};
-    categories.forEach(cat => categoryObj[cat] = []);
-    visiblePendingItems.forEach(i => categoryObj[i.category].push(i));
+    categories.forEach((cat) => (categoryObj[cat] = []));
+    visiblePendingItems.forEach((i) => categoryObj[i.category].push(i));
     setItemsByCategory(categoryObj);
   }, [visiblePendingItems]);
 
   // Handle input change.
-  const handleChange = input => {
+  const handleChange = (input) => {
     setInputValue(input);
     if (!input) {
       setVisiblePendingItems(pendingItems);
     } else {
       // Filter pending items for those that match search.
-      const matchingPending = pendingItems.filter(item => item.label.includes(input));
+      const matchingPending = pendingItems.filter((item) =>
+        item.label.includes(input)
+      );
       setVisiblePendingItems(matchingPending);
     }
-  }
+  };
 
   // Handle adding new item.
   const handleAdd = () => {
-    setPendingItems([...pendingItems, {label: inputValue, qty: 1, price: 0, category: 'uncategorized'}]);
-    setInputValue('');
-  } 
+    setPendingItems([
+      ...pendingItems,
+      { label: inputValue, qty: 1, price: 0, category: "uncategorized", id: itemCount},
+    ]);
+    setItemCount(itemCount + 1);
+    setInputValue("");
+  };
 
   return (
     <Wrapper>
       <H1>Awesome Shopping List App</H1>
       <FlexSection>
         {/* Input and subtotal go at top */}
-        <Input 
-          value={inputValue} 
-          placeholder="Item name" 
-          onChange={(e)  => handleChange(e.target.value)}
-          onKeyDown={e => {if (e.key === "Enter") handleAdd()}}
+        <Input
+          value={inputValue}
+          placeholder="Item name"
+          onChange={(e) => handleChange(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") handleAdd();
+          }}
         />
         <Button onClick={() => handleAdd()}>Create</Button>
         Subtotal (USD): $0.00
@@ -68,34 +77,35 @@ function App() {
       <FlexSection>
         <FlexItem>
           <H2>Pending</H2>
-          {categories.map(cat => {
-            return itemsByCategory[cat].length && (
-              <ListGroup 
-                title={cat} 
-                items={itemsByCategory[cat]} 
-                itemClickHandler={(item) => {
-                  setPendingItems(pendingItems.filter(i => i !== item));
-                  setRemovedItems([...removedItems, item]);
-                }} 
-                itemEditHandler={(item) => console.log(item)}
-              />
+          {categories.map((cat) => {
+            return (
+              itemsByCategory[cat].length && (
+                <ListGroup
+                  key={cat}
+                  title={cat}
+                  items={itemsByCategory[cat]}
+                  itemClickHandler={(item) => {
+                    setPendingItems(pendingItems.filter((i) => i !== item));
+                    setRemovedItems([...removedItems, item]);
+                  }}
+                  itemEditHandler={(item) => console.log(item)}
+                />
+              )
             );
           })}
         </FlexItem>
         <FlexItem>
           <H2>Crossed Off</H2>
-          <ListGroup 
+          <ListGroup
             title=""
-            items={removedItems} 
+            items={removedItems}
             itemClickHandler={(item) => {
-              setRemovedItems(removedItems.filter(i => i !== item));
+              setRemovedItems(removedItems.filter((i) => i !== item));
               setPendingItems([...pendingItems, item]);
-            }} 
-            itemEditHandler={(item) => console.log(item)}
+            }}
           />
         </FlexItem>
       </FlexSection>
-
     </Wrapper>
   );
 }
--- src/components/ListGroup.js
+++ src/components/ListGroup.js
@@ -1,4 +1,6 @@
-import styled from 'styled-components';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
 const Wrapper = styled.div``;
 const H3 = styled.h3``;
@@ -7,22 +9,42 @@ const ListItem = styled.li``;
 const EditButton = styled.button``;
 
 function ListGroup({ title, items, itemClickHandler, itemEditHandler }) {
+
+    const sortByLabel = (a,b) => {
+        const labelA = a.label.toUpperCase();
+        const labelB = b.label.toUpperCase();
+        return (labelA > labelB) ? 1 : ((labelB > labelA) ? -1 : 0);
+    }
+
   return (
     <Wrapper>
-        <H3>{title}</H3>
-        <List>
-            {items.map(item => (
-                <>
-                <ListItem onClick={() => itemClickHandler(item)}>
-                    {item.qty} {item.label} {item.price}
-                </ListItem>
-                <EditButton onClick={() => itemEditHandler(item)}>edit</EditButton>
-                </>
-            ))
-            }
-        </List>
+      <H3>{title}</H3>
+      <List>
+        {items.sort(sortByLabel).map((item) => (
+          <React.Fragment key={item.id}>
+            <ListItem onClick={() => itemClickHandler(item)}>
+              {item.qty} {item.label} ${item.price}.00
+            </ListItem>
+            <EditButton onClick={() => itemEditHandler(item)}>edit</EditButton>
+          </React.Fragment>
+        ))}
+      </List>
     </Wrapper>
   );
 }
+
+ListGroup.propTypes = {
+    title: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.object),
+    itemClickHandler: PropTypes.func,
+    itemEditHandler: PropTypes.func,
+};
+
+ListGroup.defaultProps = {
+    title: "",
+    items: [],
+    itemClickHandler: () => {},
+    itemEditHandler: () => {},
+};
 
 export default ListGroup;
